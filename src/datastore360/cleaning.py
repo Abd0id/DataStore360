9c4d27f688cff545a35f773aa88bd7673c8116a6
Abd0id <abdellah.idfath.dev@gmail.com>
2026-09-20 14:16:37 +0000

    data cleaning notebook

--- src/datastore360/cleaning.py
+++ src/datastore360/cleaning.py
@@ -2,8 +2,16 @@ import pandas as pd
 import numpy as np
 
 
-file_path = '../../data/raw/store-data-6aa6d7a3f171f140353680.csv'
-df = pd.read_csv(file_path)
+csv_path = "../../data/raw/store-data-6aa6d7a3f171f140353680.csv"
 
-df_cleaning = df.drop_duplicates()
+df = pd.read_csv(csv_path)
 
+str_cols = df.select_dtypes(include="object").columns
+df[str_cols] = df[str_cols].apply(lambda s: s.str.strip())
+
+df["Category"] = df["Category"].str.lower()
+df["Segment"] = df["Segment"].str.lower()
+
+df["Segment"] = df["Segment"].replace({"Consumerr": "Consumer"})
+
+# print(df["Segment"].unique())
